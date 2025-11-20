e14b6a6e60e268b7b328e4aefef2a09ad59f0a59
willy161 <gasper.jezovsek@gmail.com>
2025-11-20 18:53:32 +0100

    Maven structure

---
+++ src/main/java/com/golfapp/shramba/ShrambaApplication.java
@@ -0,0 +1,13 @@
+package com.golfapp.shramba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShrambaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShrambaApplication.class, args);
+	}
+
+}
